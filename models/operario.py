fc25d5632dcf7be22cf4a69c51609af67e804c38
Capitan77 <dael202106@gmail.com>
2025-10-04 19:20:09 -0500

    mensajes

--- models/operario.py
+++ models/operario.py
@@ -4,6 +4,12 @@
 from models.base_empleado import Empleado
 
 class Operario(Empleado):
+
+    #al no colocar el def __init__, de igual manera como si lo pusieramos los
+    #atributos de la clase padre ya quedan inicializados
+
+    #declaramos un metodo que retorne unicamente la definicion del cargo
+    #que esta desempeñando el empleado
     def get_cargo(self):
         return "Operario"
 
--- repository/repositorio.py
+++ repository/repositorio.py
@@ -5,23 +5,24 @@ class EmpleadosRepo:
     def __init__(self):
         # Diccionario: clave = DNI, valor = instancia de Empleado (o subclase)
         self.empleados = {}
-
+    
+    #necesitamos de un parametro que retorne al empleado para agregarlo a la lista
     def agregar(self, empleado):
-        """
-        Inserta un empleado nuevo. Lanza error si el DNI ya existe.
-        """
+        
+        #Inserta un empleado nuevo. Lanza error si el DNI ya existe.
+        
         if empleado.get_dni() in self.empleados:
             raise ValueError("Ya existe un empleado con ese DNI.")
         self.empleados[empleado.get_dni()] = empleado
 
     def obtener(self, dni):
-        """
-        Devuelve el empleado por DNI, o None si no existe.
-        """
+       
+        #Devuelve el empleado por DNI, o None si no existe.
+       
         return self.empleados.get(dni)
 
     def listar(self):
-        """
-        Retorna la lista (copia) de empleados almacenados.
-        """
+       
+        #Retorna la lista (copia) de empleados almacenados.
+        
         return list(self.empleados.values())
